2ca408030f3d312136c18631af1dbfceb72503d7
hazz41 <25110149@htu.edu.jo>
2025-12-31 13:46:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const button=document.getElementById("btn");
+const body=document.getElementById("test");
+button.addEventListener("click", function() {
+ body.style.backgroundColor="lightblue";   
+    
+});
+
